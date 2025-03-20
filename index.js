fd48c8362ef31f93b824f2253d0f7e32b724006f
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-03-20 10:30:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+
+const mongoUrl = "mongodb://localhost:27017/myDatabase";
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.get("/", async (req, res) => {
+  const users = await User.find();
+
+  console.log(users);
+
+  res.status(200).send({
+    data: users,
+  });
+});
+
+app.post("/user", async (req, res) => {
+  const { username, password } = req.body;
+
+  const newUser = new User({ username, password });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).send({ message: "User created", user: savedUser });
+  } catch (err) {
+    res.status(500).send({ message: "Error creating user", error: err });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
